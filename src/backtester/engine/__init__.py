e50ac2445f364f7cfc68c7bb2111bcc710518064
Alfio Osma <alfio32osma@gmail.com>
2026-08-28 12:56:44 +0000

    reorginazed directories

---
+++ src/backtester/engine/__init__.py
@@ -0,0 +1,3 @@
+from src.backtester.engine.engine import run_simulation_engine
+
+__all__ = ["run_simulation_engine"]
--- src/backtester/runner.py
+++
@@ -1,4 +0,0 @@
-from src.backtester.risk_manager import evaluate_protection_system
-from src.backtester.position import TradePosition
-
-def run_simulation
